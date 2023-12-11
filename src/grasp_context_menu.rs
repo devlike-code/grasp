30f50818cc7e610d58e87cb745e04cfb647f291c
Gavrilov Miroslav <gavrilov.miroslav@gmail.com>
2023-12-11 21:16:24 +0100

    Reintegrated context menus

--- src/grasp_context_menu.rs
+++ src/grasp_context_menu.rs
@@ -2,8 +2,13 @@ use std::ops::Sub;
 
 use egui::Ui;
 use mosaic::{
-    capabilities::{CollageExportCapability, QueueCapability},
-    internals::{arrows_from, targets_from, tiles, MosaicIO},
+    capabilities::{
+        ArchetypeSubject, CollageExportCapability, GroupingCapability, QueueCapability,
+        SelectionCapability,
+    },
+    internals::{
+        arrows_from, descriptors_from, gather, par, take_components, targets_from, tiles, MosaicIO,
+    },
     iterators::{component_selectors::ComponentSelectors, tile_getters::TileGetters},
 };
 
@@ -28,22 +33,69 @@ impl GraspEditorTab {
 
     // menu to show when having selection
     fn show_selection_menu(&mut self, ui: &mut Ui) {
-        if ui.button("Filter: My Neighbors").clicked() {
-            if let Some(queue) = self
-                .document_mosaic
-                .get_all()
-                .include_component("NewTabRequestQueue")
-                .get_targets()
-                .next()
-            {
-                self.document_mosaic.enqueue(
-                    &queue,
-                    &targets_from(arrows_from(tiles(self.editor_data.selected.clone())))
-                        .to_tiles(&self.document_mosaic),
-                );
+        ui.menu_button("Filter", |ui| {
+            if ui.button("Select").clicked() {
+                if let Some(queue) = self
+                    .document_mosaic
+                    .get_all()
+                    .include_component("NewTabRequestQueue")
+                    .get_targets()
+                    .next()
+                {
+                    let selection_tile = self.document_mosaic.make_selection();
+                    self.document_mosaic
+                        .fill_selection(&selection_tile, &self.editor_data.selected.clone());
+
+                    let c1 = targets_from(take_components(
+                        &["Group"],
+                        arrows_from(descriptors_from(tiles(vec![selection_tile.clone()]))),
+                    ));
+
+                    let c2 = arrows_from(targets_from(take_components(
+                        &["Group"],
+                        arrows_from(descriptors_from(tiles(vec![selection_tile.clone()]))),
+                    )));
+
+                    let c = gather(vec![c1, c2]);
+                    let tile = c.to_tiles(&self.document_mosaic);
+                    tile.add_component("Label", par("Selection"));
+
+                    self.document_mosaic.enqueue(&queue, &tile);
+                    self.document_mosaic.request_quadtree_update();
+                }
+
+                self.exit_menu(ui);
             }
-            ui.close_menu();
-        }
+
+            if ui.button("Group - todo").clicked() {
+                //self.document_mosaic.get_group_members(group, tile)
+                self.exit_menu(ui);
+            }
+
+            if ui.button("First Neigbours").clicked() {
+                if let Some(queue) = self
+                    .document_mosaic
+                    .get_all()
+                    .include_component("NewTabRequestQueue")
+                    .get_targets()
+                    .next()
+                {
+                    // check issue with the vec<&Tile>
+                    let c1 = targets_from(arrows_from(tiles(self.editor_data.selected.clone())));
+                    let c2 = tiles(self.editor_data.selected.clone());
+                    let c3 = arrows_from(tiles(self.editor_data.selected.clone()));
+
+                    let c = gather(vec![c1, c2, c3]);
+                    let tile = c.to_tiles(&self.document_mosaic);
+                    tile.add_component("Label", par("First Neighbour"));
+
+                    self.document_mosaic.enqueue(&queue, &tile);
+                    self.document_mosaic.request_quadtree_update();
+                }
+
+                self.exit_menu(ui);
+            }
+        });
     }
 
     // default context menu
